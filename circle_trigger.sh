55f47cae49dfab0a07c16dc2a3b8d2fb15f7c011
enricjorda <enric.jorda@codurance.com>
2019-09-23 15:40:46 +0200

    url test

--- circle_trigger.sh
+++ circle_trigger.sh
@@ -84,6 +84,7 @@ echo "Triggering pipeline with data:"
 echo -e "  $DATA"
 
 URL="${CIRCLE_API}/v2/project/${REPOSITORY_TYPE}/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/pipeline"
+echo -e " $URL"
 HTTP_RESPONSE=$(curl -s -u ${CIRCLE_TOKEN}: -o response.txt -w "%{http_code}" -X POST --header "Content-Type: application/json" -d "$DATA" $URL)
 
 if [ $HTTP_RESPONSE != "202" ]; then
