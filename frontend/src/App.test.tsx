ff586e16f2fe05a2074b7affe0ff08d4c71f8acb
codurance2 <mattgray83@hotmail.co.uk>
2019-09-23 12:49:48 +0100

    Add hello world

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+enzyme.configure({adapter: new Adapter()})
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('Displays "Hello World!"', () =>{
+  const wrapper = enzyme.shallow(<App/>);
+  const helloWorldElement = wrapper.find(".Hello-World");
+  expect(helloWorldElement.text()).toBe("Hello World!");
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,17 +7,9 @@ const App: React.FC = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className = "Hello-World">
+          Hello World!
+        </div>
       </header>
     </div>
   );
