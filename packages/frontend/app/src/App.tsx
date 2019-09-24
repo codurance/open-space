5093fd7938e8b05d6054e64f7393780f3ea98791
takumab <takumab@gmail.com>
2019-09-24 11:31:34 +0100

    Extract session form into it's own component

--- packages/frontend/app/src/App.tsx
+++ packages/frontend/app/src/App.tsx
@@ -1,23 +1,17 @@
+<<<<<<< HEAD
 import React, { useState } from "react";
 import SessionForm from "./session/SessionForm";
 import Sessions from "./session/Sessions";
 import logo from "./logo.svg";
 import "./App.css";
+=======
+import React, { useState } from 'react';
+import SessionForm from './session/SessionForm';
+import logo from './logo.svg';
+import './App.css';
+>>>>>>> eda1068e... Extract session form into it's own component
 
 const App: React.FC = () => {
-  const [sessionTitle, setSessionTitle] = useState("");
-  const [sessionLocation, setSessionLocation] = useState("");
-  const [sessionTime, setSessionTime] = useState("");
-  const [sessionPresenter, setSessionPresenter] = useState("");
-
-  const postSession = (event: React.FormEvent) => {
-    event.preventDefault()
-    console.log(sessionTitle);
-    console.log(sessionLocation);
-    console.log(sessionTime);
-    console.log(sessionPresenter);
-  }
-
   return (
     <div className="App">
       <header className="App-header">
