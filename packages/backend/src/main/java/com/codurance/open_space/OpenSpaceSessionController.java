c7e3ae4fff725fc9951495a49f8dba4ae47ed6bf
Ivan Katzarski <10603133+ikatzarski@users.noreply.github.com>
2019-09-24 17:05:44 +0100

    Added POST request for creating a OpenSpaceSession. Added a test for the POST.

--- packages/backend/src/main/java/com/codurance/open_space/OpenSpaceSessionController.java
+++ packages/backend/src/main/java/com/codurance/open_space/OpenSpaceSessionController.java
@@ -1,15 +1,15 @@
 package com.codurance.open_space;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static org.springframework.http.HttpStatus.*;
+
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("sessions")
+@RequestMapping("/api/sessions")
 public class OpenSpaceSessionController {
 
     private final OpenSpaceSessionRepository repository;
@@ -17,5 +17,11 @@ public class OpenSpaceSessionController {
     @GetMapping
     public List<OpenSpaceSession> getAllOpenSpaceSessions() {
         return repository.findAll();
+    }
+
+    @ResponseStatus(CREATED)
+    @PostMapping
+    public OpenSpaceSession create(@RequestBody OpenSpaceSession openSpaceSession) {
+        return repository.save(openSpaceSession);
     }
 }
--- packages/backend/src/test/java/com/codurance/open_space/OpenSpaceSessionControllerShould.java
+++ packages/backend/src/test/java/com/codurance/open_space/OpenSpaceSessionControllerShould.java
@@ -1,5 +1,6 @@
 package com.codurance.open_space;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -11,6 +12,7 @@ import java.util.List;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(OpenSpaceSessionController.class)
@@ -23,7 +25,7 @@ public class OpenSpaceSessionControllerShould {
     private OpenSpaceSessionRepository repository;
 
     @Test
-    void return_all_open_space_sessions_from_the_h2_db() throws Exception {
+    void get_all_open_space_sessions_from_the_repository() throws Exception {
         when(repository.findAll()).thenReturn(
                 List.of(new OpenSpaceSession(
                         1,
@@ -32,11 +34,51 @@ public class OpenSpaceSessionControllerShould {
                         "11:00",
                         "David")));
 
-        mockMvc.perform(get("/sessions")
+        mockMvc.perform(get("/api/sessions")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content()
                         .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(content().json("[{\"id\":1,\"title\":\"Session 1\",\"location\":\"Location 1\",\"time\":\"11:00\",\"presenter\":\"David\"}]"));
+    }
+
+    @Test
+    void post_open_space_session() throws Exception {
+        when(repository.save(new OpenSpaceSession(
+                1,
+                "Session 1",
+                "Location 1",
+                "11:00",
+                "David")))
+                .thenReturn(new OpenSpaceSession(
+                        1,
+                        "Session 1",
+                        "Location 1",
+                        "11:00",
+                        "David"));
+
+        mockMvc.perform(post("/api/sessions")
+                .content(asJsonString(new OpenSpaceSession(
+                        1,
+                        "Session 1",
+                        "Location 1",
+                        "11:00",
+                        "David")))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.title").value("Session 1"))
+                .andExpect(jsonPath("$.location").value("Location 1"))
+                .andExpect(jsonPath("$.time").value("11:00"))
+                .andExpect(jsonPath("$.presenter").value("David"));
+    }
+
+    private static String asJsonString(final Object object) {
+        try {
+            return new ObjectMapper().writeValueAsString(object);
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
     }
 }
