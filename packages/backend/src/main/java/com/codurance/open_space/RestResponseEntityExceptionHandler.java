5929c4bd67137686e941756b645743cfa6cc1a01
Andrei Bogomja <bogomja@gmail.com>
2019-09-25 16:45:03 +0100

    Added @ControllerAdvice for handling exceptions in OpenSpaceSessionController

---
+++ packages/backend/src/main/java/com/codurance/open_space/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.codurance.open_space;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.persistence.EntityNotFoundException;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {EntityNotFoundException.class, EmptyResultDataAccessException.class})
+    protected ResponseEntity<Object> handleIdNotFound(RuntimeException runtimeException, WebRequest request) {
+        return handleExceptionInternal(runtimeException, "Id not found", new HttpHeaders(), NOT_FOUND, request);
+    }
+}
