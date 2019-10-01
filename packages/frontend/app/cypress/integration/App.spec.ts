a8fde81d22cd090c438e69bbae16dfd6e3cd5cd5
Sherlock Qiao <sherlockq@gmail.com>
2019-10-01 16:20:14 +0100

    Fix existing e2e test for submitting a session

--- packages/frontend/app/cypress/integration/App.spec.ts
+++ packages/frontend/app/cypress/integration/App.spec.ts
@@ -1,18 +1,18 @@
-describe('OPEN-SPACE e2e', function() {
+describe('OPEN-SPACE e2e', function () {
 
-  it('Submit a session', function() {
+  it('Submit a session', function () {
     cy.visit('/')
-    cy.get('label').contains('Title:').find('input').first().type('Serverless');
-    cy.get('label').contains('Location:').find('input').first().type('Location 3');
-    cy.get('label').contains('Presenter:').find('input').first().type('Enric');
-    cy.get('label').contains('Time:').find('input').first().type('15:00');
+    cy.get('label').contains('Title').next('input').first().type('Serverless');
+    cy.get('label').contains('Location').next('input').first().type('Location 3');
+    cy.get('label').contains('Presenter').next('input').first().type('Enric');
+    cy.get('label').contains('Time').next('input').first().type('15:00');
     return cy.get('form').submit();
   })
-  
-  
-  it('Delete submitted session', function() {
+
+  it('Delete submitted session', function () {
     cy.get('div').contains('Enric').parent().as('conferenceDiv');
     cy.get('@conferenceDiv').find('button.delete-session').as('buttonDelete');
     return cy.get('@buttonDelete').click();
   })
+
 })
