c67394a80f68c5a85bdd447524d5e52a4c527a2d
David Halewood <davidnhalewood@hotmail.com>
2019-10-02 13:48:11 +0100

    Use github whatwg-fetch instead of javascrip fetch
    This is so that cypress can pick up the XHR requests that whatwg-fetch
    uses under the hood

--- packages/frontend/app/cypress/integration/App.spec.ts
+++ packages/frontend/app/cypress/integration/App.spec.ts
@@ -1,5 +1,7 @@
 describe("OPEN-SPACE e2e", function() {
   it("Submit a session", function() {
+    cy.server();
+    cy.route("/api/sessions").as("postSession");
     cy.visit("/");
     cy.get("label")
       .contains("Title")
@@ -21,16 +23,51 @@ describe("OPEN-SPACE e2e", function() {
       .next("input")
       .first()
       .type("15:00");
-    return cy.get("form").submit();
+
+    cy.get("form").submit();
+
+    cy.wait("@postSession");
   });
-  // FIXME
-  // remove this test for now since it's not stable enough
-  // if not wait for a response after submitting session
-  /*
-  it('Delete submitted session', function () {
-    cy.get('div').contains('Enric').parent().as('conferenceDiv');
-    cy.get('@conferenceDiv').find('button.delete-session').as('buttonDelete');
-    return cy.get('@buttonDelete').click();
-  })
-*/
+
+  it("Edit a session", function() {
+    cy.get("div.description")
+      .contains("Enric")
+      .closest(".session")
+      .as("sessionDiv");
+    cy.get("@sessionDiv")
+      .next(".edit-session")
+      .as("edit");
+    cy.get("@edit").click();
+
+    // assert after clicking edit button, form shows present values
+    cy.get("label")
+      .contains("Title")
+      .next("input")
+      .should("have.value", "Serverless");
+    cy.get("label")
+      .contains("Location")
+      .next("input")
+      .should("have.value", "Location 3");
+    cy.get("label")
+      .contains("Presenter")
+      .next("input")
+      .should("have.value", "Enric");
+    cy.get("label")
+      .contains("Time")
+      .next("input")
+      .should("have.value", "15:00");
+
+    // return cy.get('form').submit();
+  });
+
+  it("Delete submitted session", function() {
+    cy.get("div")
+      .contains("Enric")
+      .parent()
+      .as("conferenceDiv");
+    cy.get("@conferenceDiv")
+      .find("button.delete-session")
+      .as("buttonDelete");
+    return cy.get("@buttonDelete").click();
+  });
 });
