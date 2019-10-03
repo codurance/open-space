beb91b7830f5387241b9421034a24699080afdd5
Sherlock Qiao <sherlockq@gmail.com>
2019-10-03 15:43:20 +0100

    add edit button to test, and clean with beforeeach

--- packages/frontend/app/src/session/Session.test.tsx
+++ packages/frontend/app/src/session/Session.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Session } from "./Session";
-import enzyme from "enzyme";
+import enzyme, { ShallowWrapper } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import { Button } from "semantic-ui-react";
 enzyme.configure({ adapter: new Adapter() });
@@ -11,8 +11,9 @@ const location = "location";
 const presenter = "presenter";
 const time = "12:00";
 
-it("Displays session", () => {
-  const wrapper = enzyme.shallow(
+let wrapper: ShallowWrapper;
+beforeEach(() => {
+  wrapper = enzyme.shallow(
     <Session
       id={id}
       title={title}
@@ -23,34 +24,36 @@ it("Displays session", () => {
       isEditing={false}
     />
   );
+});
+
+it("Displays session", () => {
   const sessionElement = wrapper.find({ className: "session" });
+
   expect(sessionElement.find({ className: "session-title" }).html()).toContain(
     title
   );
+
   expect(
     sessionElement.find({ className: "session-presenter" }).html()
   ).toContain(presenter);
+
   expect(
     sessionElement.find({ className: "session-extra-details" }).html()
   ).toContain(location);
+
   expect(
     sessionElement.find({ className: "session-extra-details" }).html()
   ).toContain(time);
 });
 
 it("Displays delete button", () => {
-  const wrapper = enzyme.shallow(
-    <Session
-      id={id}
-      title={title}
-      presenter={presenter}
-      location={location}
-      time={time}
-      onEditClicked={jest.fn()}
-      isEditing={false}
-    />
-  );
   const sessionElement = wrapper.find(".delete-session");
+
+  expect(sessionElement.type()).toBe(Button);
+});
+
+it("Displays edit button", () => {
+  const sessionElement = wrapper.find(".edit-session");
 
   expect(sessionElement.type()).toBe(Button);
 });
