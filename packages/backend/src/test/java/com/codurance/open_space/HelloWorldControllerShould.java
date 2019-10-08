101c2b5784abdcd2eb2fd0920f293d003bedfb34
Andrei Bogomja <bogomja@gmail.com>
2019-10-08 12:15:51 +0100

    Reinstates previously deleted HelloWorld API

---
+++ packages/backend/src/test/java/com/codurance/open_space/HelloWorldControllerShould.java
@@ -0,0 +1,29 @@
+package com.codurance.open_space;
+
+import com.codurance.open_space.helloworld.HelloWorldController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(HelloWorldController.class)
+class HelloWorldControllerShould {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void returnHelloWorld() throws Exception {
+        mockMvc.perform(get("/api")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("text", is("Hello, world!")));
+    }
+}
