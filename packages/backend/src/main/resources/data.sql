53131eb64e9677db025e5cc5eabafd1c4ecbdff7
Andrei Bogomja <bogomja@gmail.com>
2019-10-08 10:36:39 +0100

    Adds initial development data for "spaces" table

--- packages/backend/src/main/resources/data.sql
+++ packages/backend/src/main/resources/data.sql
@@ -9,3 +9,9 @@ values ('Session 3', 'Location 3', '14:00', 'Sherlock');
 
 insert into sessions (title, location, time, presenter)
 values ('Session 4', 'Location 4', '14:00', 'Tacuma');
+
+insert into spaces (name, description, location)
+values ('Name 1', 'Big size space with 10 chairs, aircon, and TV', '3rd floor');
+
+insert into spaces (name, description, location)
+values ('Name 2', 'Tiny room with no windows', '4th floor');
