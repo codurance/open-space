1415bc4c7707e6570bbe788017a6e25ad592c19d
Sherlock Qiao <sherlockq@gmail.com>
2019-10-09 11:12:21 +0100

    update data initialization from location to location_id

--- packages/backend/src/main/resources/data.sql
+++ packages/backend/src/main/resources/data.sql
@@ -1,17 +1,18 @@
-insert into sessions (title, location, time, presenter)
-values ('Session 1', 'Location 1', '11:00', 'David');
+insert into spaces (id, name, description, location)
+values (1, 'Name 1', 'Big size space with 10 chairs, aircon, and TV', '3rd floor');
 
-insert into sessions (title, location, time, presenter)
-values ('Session 2', 'Location 2', '13:00', 'Andrei');
+insert into spaces (id, name, description, location)
+values (2, 'Name 2', 'Tiny room with no windows', '4th floor');
 
-insert into sessions (title, location, time, presenter)
-values ('Session 3', 'Location 3', '14:00', 'Sherlock');
+insert into sessions (title, location_id, time, presenter)
+values ('Session 1', 1, '11:00', 'David');
 
-insert into sessions (title, location, time, presenter)
-values ('Session 4', 'Location 4', '14:00', 'Tacuma');
+insert into sessions (title, location_id, time, presenter)
+values ('Session 2', 1, '13:00', 'Andrei');
 
-insert into spaces (name, description, location)
-values ('Name 1', 'Big size space with 10 chairs, aircon, and TV', '3rd floor');
+insert into sessions (title, location_id, time, presenter)
+values ('Session 3', 2, '14:00', 'Sherlock');
 
-insert into spaces (name, description, location)
-values ('Name 2', 'Tiny room with no windows', '4th floor');
+insert into sessions (title, location_id, time, presenter)
+values ('Session 4', 2, '14:00', 'Tacuma');
+
