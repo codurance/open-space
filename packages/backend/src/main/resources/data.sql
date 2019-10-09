ebcfccbbbedb1d067d44cda5c24fd4a113a21b50
Sherlock Qiao <sherlockq@gmail.com>
2019-10-09 14:58:51 +0100

    Add type to data initialization

--- packages/backend/src/main/resources/data.sql
+++ packages/backend/src/main/resources/data.sql
@@ -4,15 +4,15 @@ values (1, 'Name 1', 'Big size space with 10 chairs, aircon, and TV', '3rd floor
 insert into spaces (id, name, description, location)
 values (2, 'Name 2', 'Tiny room with no windows', '4th floor');
 
-insert into sessions (title, location_id, time, presenter)
-values ('Session 1', 1, '11:00', 'David');
+insert into sessions (title, location_id, time, presenter, type)
+values ('Session 1', 1, '11:00', 'David', 'Round Table');
 
-insert into sessions (title, location_id, time, presenter)
-values ('Session 2', 1, '13:00', 'Andrei');
+insert into sessions (title, location_id, time, presenter, type)
+values ('Session 2', 1, '13:00', 'Andrei', 'Lecture');
 
-insert into sessions (title, location_id, time, presenter)
-values ('Session 3', 2, '14:00', 'Sherlock');
+insert into sessions (title, location_id, time, presenter, type)
+values ('Session 3', 2, '14:00', 'Sherlock', 'Mobbing');
 
-insert into sessions (title, location_id, time, presenter)
-values ('Session 4', 2, '14:00', 'Tacuma');
+insert into sessions (title, location_id, time, presenter, type)
+values ('Session 4', 2, '14:00', 'Tacuma', 'Round Table');
 
