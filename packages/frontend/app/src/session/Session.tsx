3d8e003aaaba7598f3bfa0ca07fad75aa8af71ca
Andrei Bogomja <bogomja@gmail.com>
2019-10-10 14:33:29 +0100

    Add functionality to confirm before deleting a session

--- packages/frontend/app/src/session/Session.tsx
+++ packages/frontend/app/src/session/Session.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from "react";
 import { deleteSession } from "../common/http";
-import { Button, Card, Icon } from "semantic-ui-react";
+import { Button, Card, Icon, Modal } from "semantic-ui-react";
 import * as sessionsLocalStorage from "../common/sessionsLocalStorage";
 import SessionsContext, { ISession } from "./sessionsContext";
 import * as sessionAPI from "./api/sessionAPI";
@@ -24,6 +24,8 @@ export const Session = ({
     SessionsContext
   );
 
+  const [open, setOpen] = useState(false);
+
   const [interest, setInterest] = useState(
     sessionsLocalStorage.checkInterest(id)
   );
@@ -45,32 +47,44 @@ export const Session = ({
   };
 
   return (
-    <Card className="session" fluid>
-      <Card.Content>
-        <Card.Header className="session-title">{title}</Card.Header>
-        <Card.Description className="session-presenter">
-          <Icon name="user" />
-          {presenter}
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra className="session-extra-details">
-        {location} @ {time}
-      </Card.Content>
-      <Card.Content>
-        <Button icon onClick={() => toggleInterest()}>
-          <Icon name={interest ? "heart" : "heart outline"} />
-        </Button>
-        <Button
-          className="delete-session"
-          icon
-          onClick={() => deleteSessionById(id)}
-        >
-          <Icon name="trash" />
-        </Button>
-        <Button className="edit-session" onClick={() => onEditClicked(id)}>
-          Edit
-        </Button>
-      </Card.Content>
-    </Card>
+    <div>
+      <Card className="session" fluid>
+        <Card.Content>
+          <Card.Header className="session-title">{title}</Card.Header>
+          <Card.Description className="session-presenter">
+            <Icon name="user" />
+            {presenter}
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra className="session-extra-details">
+          {location} @ {time}
+        </Card.Content>
+        <Card.Content>
+          <Button icon onClick={() => toggleInterest()}>
+            <Icon name={interest ? "heart" : "heart outline"} />
+          </Button>
+          <Button className="delete-session" icon onClick={() => setOpen(true)}>
+            <Icon name="trash" />
+          </Button>
+          <Button className="edit-session" onClick={() => onEditClicked(id)}>
+            Edit
+          </Button>
+        </Card.Content>
+      </Card>
+      <Modal open={open}>
+        <Modal.Header>Delete Session</Modal.Header>
+        <Modal.Content>
+          <p>Are you sure you want to delete this session?</p>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button negative onClick={() => setOpen(false)}>
+            No
+          </Button>
+          <Button positive onClick={() => deleteSessionById()}>
+            Yes
+          </Button>
+        </Modal.Actions>
+      </Modal>
+    </div>
   );
 };
